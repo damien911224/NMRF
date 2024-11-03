fd32208631629c0f4bf02b82607e823190f53057
damien <damien911224@gmail.com>
2024-11-03 16:35:28 +0900

    feat: concat image handling

---
+++ get_depth.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+
+np_path = "/mnt/hdd0/NMRF/kitti/1729059831.8904958.npy"
+disparity = np.load(np_path)
+
+B = 120 # mm
+f = 2.1 # 2.1 or 4 mm
+
+# depth = Baseline * focal-lens / disparity
+depth = B * f / disparity
+
+print(disparity)
+
+print()
+
+print(depth)
