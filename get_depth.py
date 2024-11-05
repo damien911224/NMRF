ec9ee0ab14f69b9ceaa785bf45d315eb56ef960f
damien <damien911224@gmail.com>
2024-11-05 17:38:52 +0900

    feat: concat image handling

--- get_depth.py
+++ get_depth.py
@@ -51,47 +51,50 @@ def disparity_to_depth(disparity_map, focal_lengths_pixels, baseline_meters,
 
     return depth_map
 
+def depth_to_3d(depth_map, focal_lengths_pixels, camera_positions_pixels):
+    # 깊이 맵의 높이와 너비를 가져옵니다.
+    height, width = depth_map.shape
+    fx, fy = focal_lengths_pixels
+    cx, cy = camera_positions_pixels
+
+    # 3D 포인트를 저장할 배열을 초기화합니다.
+    points_3d = np.zeros((height, width, 3), dtype=np.float32)
+
+    for v in range(height):
+        for u in range(width):
+            Z = depth_map[v, u]  # 깊이 값 (미터 단위)
+
+            # 깊이 값이 0이 아닌 경우에만 3D 포인트 계산
+            if Z > 0:
+                X = (u - cx) * Z / fx
+                Y = (v - cy) * Z / fy
+                points_3d[v, u] = np.array([X, Y, Z])  # X, Y, Z는 미터 단위
+
+    return points_3d
+
+import open3d as o3d
 import numpy as np
-import matplotlib.pyplot as plt
-import cv2
-from mpl_toolkits.mplot3d import Axes3D
 
-def pixel_to_3d(x, y, depth, focal_length, cx, cy):
-    Z = depth
-    X = (x - cx) * Z / focal_length
-    Y = (y - cy) * Z / focal_length
-    return np.array([X, Y, Z])
+filename = "20241014_data_labeling/sub_dir1/points/real_240930-86.npy"
+points = np.load(filename, allow_pickle=True)
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(points)
 
-def get_3d_bbox_from_2d(bbox_2d, depth_map, focal_length, cx, cy):
-    x_min, y_min, x_max, y_max = bbox_2d
+max_bound = np.max(points, axis=0)
+min_bound = np.min(points, axis=0)
+print("Point Cloud")
+print("shape", points.shape)
+print("max", max_bound, flush=True)
+print("min", min_bound, flush=True)
 
-    # 바운딩 박스의 네 꼭짓점에 해당하는 깊이 값 추출
-    depth_topleft = depth_map[y_min, x_min]
-    depth_topright = depth_map[y_min, x_max]
-    depth_bottomleft = depth_map[y_max, x_min]
-    depth_bottomright = depth_map[y_max, x_max]
-
-    # 2D 좌표를 3D 좌표로 변환
-    corner_3d_topleft = pixel_to_3d(x_min, y_min, depth_topleft, focal_length, cx, cy)
-    corner_3d_topright = pixel_to_3d(x_max, y_min, depth_topright, focal_length, cx, cy)
-    corner_3d_bottomleft = pixel_to_3d(x_min, y_max, depth_bottomleft, focal_length, cx, cy)
-    corner_3d_bottomright = pixel_to_3d(x_max, y_max, depth_bottomright, focal_length, cx, cy)
-
-    avg_depth = np.mean([depth_topleft, depth_topright, depth_bottomleft, depth_bottomright])
-    center_x, center_y = (x_min + x_max) // 2, (y_min + y_max) // 2
-    center_3d = pixel_to_3d(center_x, center_y, avg_depth, focal_length, cx, cy)
-
-    bbox_3d = {
-        "center": center_3d,
-        "corners": [corner_3d_topleft, corner_3d_topright, corner_3d_bottomleft, corner_3d_bottomright]
-    }
-    return bbox_3d
-
-
+print("\nVisualize result")
+coordinate_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1., origin=[0, 0, 0])
+o3d.visualization.draw_geometries([pcd, coordinate_frame])
 
 if __name__ == '__main__':
     # 카메라 파라미터 설정
-    focal_lengths_pixels = (1066.7, 1066.7) # Left sensor fx, fy 값
+    focal_lengths_pixels = (1066.7, 1066.67) # Left sensor fx, fy 값
+    camera_positions_pixels = (1137.39, 669.699)
     baseline_meters = 0.12 # baseline in meters (120mm)
 
     root_folder = os.path.join("/mnt/hdd0/NMRF/depth/npy")
@@ -101,16 +104,24 @@ if __name__ == '__main__':
     paths = glob.glob(os.path.join(root_folder, "*.npy"))
 
     for path in tqdm(paths):
+        if "crop" in path:
+            continue
+
         # disparity
         disparity_map = np.load(path).transpose(0, 1)
 
         # Depth map 변환
         depth_map = disparity_to_depth(disparity_map, focal_lengths_pixels, baseline_meters)
+        points_3d = depth_to_3d(depth_map, focal_lengths_pixels, camera_positions_pixels)
 
-        # Depth map normalization (0-255 범위로 스케일링)
-        depth_map_normalized = cv2.normalize(depth_map, None, 0, 255, cv2.NORM_MINMAX)
-        depth_map_normalized = depth_map_normalized.astype(np.uint8)
+        origin_path = glob.glob("/mnt/hdd0/stereo/*", os.path.basename(path).replace(".npy", ".png"))[0]
+        print(origin_path)
+        exit()
 
-        # Depth map 저장
-        output_path = os.path.join(output_folder, os.path.basename(path).replace(".npy", ".png"))
-        plt.imsave(output_path, depth_map_normalized, cmap='magma')
+        # # Depth map normalization (0-255 범위로 스케일링)
+        # depth_map_normalized = cv2.normalize(depth_map, None, 0, 255, cv2.NORM_MINMAX)
+        # depth_map_normalized = depth_map_normalized.astype(np.uint8)
+        #
+        # # Depth map 저장
+        # output_path = os.path.join(output_folder, os.path.basename(path).replace(".npy", ".png"))
+        # plt.imsave(output_path, depth_map_normalized, cmap='magma')
